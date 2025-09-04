9ecb753ca51661d6499c64d1029128f9f9f220c8
DeVontae <dhudso3@uwo.ca>
2025-09-04 00:25:20 -0400

    changed logo location

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/vero_landing_page" : "",
-  assetPrefix: isProd ? "/vero_landing_page/" : "",
 };
 
 module.exports = nextConfig;
-
