fa773923a3cc38bf0e6e5f1fb1c870f60c6efe06
DeVontae <dhudso3@uwo.ca>
2025-09-03 22:34:15 -0400

    updated contents for deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/vero_landing_page" : "",
+  assetPrefix: isProd ? "/vero_landing_page/" : "",
   experimental: {
     appDir: true,
   },
